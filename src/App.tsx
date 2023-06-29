a26d8f9b091693f482a94a049d22e45c4b030aa9
Gustavoph <gusta.coliveira07@gmail.com>
2023-06-29 10:12:04 -0300

    feat: add initial config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+export function App() {
+  return (
+    <Provider store={store}>
+       <h1>Hello World</h1>
+    </Provider>
+  )
+}
+
